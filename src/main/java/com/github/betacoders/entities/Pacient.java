46ac741ea4a5fe8880d30004dee1fe9add9506fc
AntCursor <antcursor@gmail.com>
2026-08-18 17:07:03 -0300

    entities: created Pacient class

---
+++ src/main/java/com/github/betacoders/entities/Pacient.java
@@ -0,0 +1,13 @@
+package com.github.betacoders.entities;
+
+import com.github.betacoders.types.Position;
+
+/**
+ * Pacient
+ */
+public class Pacient {
+  private Position pos;
+  private boolean preferential;
+  private StaticEntities target;
+  private int ticketNum;
+}
