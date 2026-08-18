db5b13b2f7476a361e28508d61e725b85470f3e9
AntCursor <antcursor@gmail.com>
2026-08-18 16:33:17 -0300

    grid: added grid class

---
+++ src/main/java/com/github/betacoders/grid/Grid.java
@@ -0,0 +1,42 @@
+package com.github.betacoders.grid;
+
+public class Grid<E extends Enum<E>> {
+  public interface CellConsumer {
+    void apply(int x, int y);
+  }
+
+  private int sizeX;
+  private int sizeY;
+
+  protected Object[] cells;
+
+  public int sizeX() {
+    return sizeX;
+  }
+
+  public int sizeY() {
+    return sizeY;
+  }
+
+  public Grid(int w, int h) {
+    this.sizeX = w;
+    this.sizeY = h;
+
+    this.cells = new Object[h * w];
+  }
+
+  public E get(int x, int y) {
+    return (E) cells[y * sizeX + x];
+  }
+
+  public void set(int x, int y, E c) {
+    cells[y * sizeX + x] = c;
+  }
+
+  public void forEach(CellConsumer f) {
+    for (int y = 0; y < sizeY; ++y)
+      for (int x = 0; x < sizeX; ++x) {
+        f.apply(x, y);
+      }
+  }
+}
