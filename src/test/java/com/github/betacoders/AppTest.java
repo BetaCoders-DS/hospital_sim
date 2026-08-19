758b9312c27b69acbd090c9851e85d7920957b0f
AntCursor <antcursor@gmail.com>
2026-08-18 21:01:08 -0300

    tests: added Ticket related tests

--- src/test/java/com/github/betacoders/AppTest.java
+++ src/test/java/com/github/betacoders/AppTest.java
@@ -1,19 +1,33 @@
 package com.github.betacoders;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
+import com.github.betacoders.entities.Pacient;
+import com.github.betacoders.types.Position;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest {
 
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+  @Nested
+  class PacientTests {
+    Pacient p;
+
+    @BeforeEach
+    void setup() {
+      p = new Pacient(new Position(10, 10), true);
     }
+
+    @Test
+    void throwsIfInvalid() {
+      assertThrows(
+          Pacient.InvalidTicketNumberException.class,
+          () -> p.giveTicketNum(-1));
+    }
+  }
 }
