ad4e000252c4db2c94203251a98ca3d31d15d862
AntCursor <antcursor@gmail.com>
2026-08-30 18:43:43 -0300

    collections: added List interface

---
+++ src/main/java/com/github/betacoders/types/collections/List.java
@@ -0,0 +1,16 @@
+package com.github.betacoders.types.collections;
+
+/**
+ * List
+ */
+public interface List<T> extends Iterable<T> {
+  T get(int i);
+
+  void set(int i, T val);
+
+  boolean isEmpty();
+
+  void add(T item);
+
+  void remove(T item);
+}
