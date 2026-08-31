b0af2691b879c15126523dc957a31f336c732234
MiguelSan-H <dev.MiguelSan@gmail.com>
2026-08-30 23:07:53 -0300

    path: criacao do arquivo e dos metodos principais do wavefront

---
+++ src/main/java/com/github/betacoders/path/Wavefront.java
@@ -0,0 +1,26 @@
+package com.github.betacoders.path;
+
+import com.github.betacoders.entities.StaticEntities;
+import com.github.betacoders.grid.Grid;
+import com.github.betacoders.types.Position;
+import com.github.betacoders.types.collections.LinkedList;
+
+/**
+ * Wavefront
+ * Calcula, a partir de um destino, a distância mínima até cada célula
+ * transitável do grid, usando busca em largura.
+ */
+public class Wavefront {
+    private static int[] DX = { 0, 0, -1, 1 };
+    private static int[] DY = { -1, 1, 0, 0};
+
+
+    public static Grid<Integer> calculate(Grid<StaticEntities> map, Position dest) {
+        Grid<Integer> dist = new Grid<>(map.sizeX(), map.sizeY());
+        return dist;
+    }
+    private static boolean walkable(StaticEntities cell) {
+        boolean sucesso = true;
+        return sucesso;
+    }
+}
