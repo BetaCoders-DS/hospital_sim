6af15b4e9abd2ee0b73748b76b436901b1203f8a
Joao-Gabriel2202 <a2025952800@teiacoltec.org>
2026-08-30 22:31:53 -0300

    metodo para inicializar o relogio

--- src/main/java/com/github/betacoders/time/RelogioSimulacao.java
+++ src/main/java/com/github/betacoders/time/RelogioSimulacao.java
@@ -1,4 +1,15 @@
 package com.github.betacoders.time;
 
 public final class RelogioSimulacao {
+    private double tempoDecorrido = 0;   // relogio logico da simulacaoo
+    private long ultimoMillis;           // ultimo instante real em que o relogio foi atualizado
+    private boolean pausado = false;     // se true, o relogio para de avancar
+
+    //Incia o relogio do zero
+    public void iniciar() {
+    tempoDecorrido = 0; //zera o tempo do relogio
+    ultimoMillis = System.currentTimeMillis();  //ancora a referencia no tempo real atual
+    pausado = false;  //se true, o relogio para de avancar
+  }
+
 }
