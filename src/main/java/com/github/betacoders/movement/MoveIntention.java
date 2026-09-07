5bef4ebf71374e6346e2c3e66892c84c248547f9
MiguelSan-H <dev.MiguelSan@gmail.com>
2026-09-07 11:23:53 -0300

    Movement: Correcao no package e no diretorio da pasta

--- src/main/java/com/github/betacoders/movement/MoveIntention.java
+++ src/main/java/com/github/betacoders/movement/MoveIntention.java
@@ -1,10 +1,10 @@
 package com.github.betacoders.movement;
 
-import com.github.betacoders.entities.Pacient;
+import com.github.betacoders.grid.Grid;
 import com.github.betacoders.types.Position;
+import com.github.betacoders.entities.Pacient;
 
-/**
- * MoveIntention
- */
-public record MoveIntention(Pacient pacient, Position next) {
+
+public record MoveIntention(Pacient Pacient, Position next) {
+    
 }
