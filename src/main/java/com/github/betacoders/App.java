6f40f44100d0d96f0ac77ba489b85847e1966ee1
Gandwit <pedro22teles@outlook.com.br>
2026-09-07 19:24:10 -0300

    app: integracao com o sketch

--- src/main/java/com/github/betacoders/App.java
+++ src/main/java/com/github/betacoders/App.java
@@ -1,10 +1,9 @@
 package com.github.betacoders;
 
-/**
- * Hello world!
- */
+import processing.core.PApplet;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        PApplet.main("com.github.betacoders.sketch.Sketch");
     }
 }
