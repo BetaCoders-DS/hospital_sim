03c4804168498aa14159f48165b232b0f5451a3d
Gandwit <pedro22teles@outlook.com.br>
2026-09-08 13:28:57 -0300

    render: implementacao do desenho do mapa

--- src/main/java/com/github/betacoders/render/ProcessingRenderer.java
+++ src/main/java/com/github/betacoders/render/ProcessingRenderer.java
@@ -7,6 +7,7 @@ import processing.core.PApplet;
 public class ProcessingRenderer implements RenderI {
 
     private final PApplet app;
+    private final int cellSize = 25;
 
     public ProcessingRenderer(PApplet app) {
         this.app = app;
@@ -15,5 +16,29 @@ public class ProcessingRenderer implements RenderI {
     @Override
     public void renderMap(Grid<StaticEntities> map) {
         app.background(255);
+
+        if (map == null) {
+            return;
+        }
+
+        for (int y = 0; y < map.sizeY(); y++) {
+            for (int x = 0; x < map.sizeX(); x++) {
+                drawCell(map.get(x, y), x, y);
+            }
+        }
+    }
+
+    private void drawCell(StaticEntities entity, int x, int y) {
+        int pixelX = x * cellSize;
+        int pixelY = y * cellSize;
+
+        if (entity instanceof StaticEntities.Wall) {
+            app.fill(60);
+        } else {
+            app.fill(230);
+        }
+
+        app.stroke(180);
+        app.rect(pixelX, pixelY, cellSize, cellSize);
     }
 }
