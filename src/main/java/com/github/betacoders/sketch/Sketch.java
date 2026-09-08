ee1acb27feddfaba3b3d3cf8fe1e70b143d43bec
Gandwit <pedro22teles@outlook.com.br>
2026-09-08 14:06:18 -0300

    sketch: integracao do carregamento de mapas

--- src/main/java/com/github/betacoders/sketch/Sketch.java
+++ src/main/java/com/github/betacoders/sketch/Sketch.java
@@ -1,11 +1,16 @@
 package com.github.betacoders.sketch;
 
+import com.github.betacoders.entities.StaticEntities;
+import com.github.betacoders.grid.Grid;
+import com.github.betacoders.maps.MapLoader;
 import com.github.betacoders.render.ProcessingRenderer;
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 
     private ProcessingRenderer renderer;
+    private MapLoader mapLoader;
+    private Grid<StaticEntities> map;
 
     @Override
     public void settings() {
@@ -15,10 +20,16 @@ public class Sketch extends PApplet {
     @Override
     public void setup() {
         renderer = new ProcessingRenderer(this);
+        mapLoader = new MapLoader(this);
+
+        if (mapLoader.load("maps/hospital1.txt")
+                && mapLoader.isValid()) {
+            map = mapLoader.getMap();
+        }
     }
 
     @Override
     public void draw() {
-        renderer.render();
+        renderer.renderMap(map);
     }
 }
